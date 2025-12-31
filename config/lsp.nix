1c764af4aff92cc526605c3a2d19cdac3a6c800f
Leonid <penguin.ognev@gmail.com>
2025-12-31 17:42:38 +0700

    fix: lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,161 +1,101 @@
+{ ... }:
 let
-  selectOpts = ''
-    {
-      behavior = cmp.SelectBehavior.Insert
-    }
-  '';
+  selectOpts = "{ behavior = cmp.SelectBehavior.Insert }";
 in
-{ self, ... }: {
-
-  # Language servers
+{
+  # Настройки LSP
   plugins.lsp = {
     enable = true;
-
     inlayHints = true;
 
+    # Привязки клавиш для работы gd, hover и прочего
+    keymaps = {
+      diagnostic = {
+        "<leader>j" = "goto_next";
+        "<leader>k" = "goto_prev";
+      };
+      lspBuf = {
+        "gd" = "definition";
+        "gD" = "references";
+        "gt" = "type_definition";
+        "gi" = "implementation";
+        "K" = "hover";
+        "<leader>rn" = "rename";
+        "<leader>ca" = "code_action";
+      };
+    };
+
     servers = {
-      # grammar checking
       harper_ls.enable = true;
-
-      # javascript / typescript
       ts_ls.enable = true;
-
-      # lua
       lua_ls.enable = true;
-
-      # rust
       rust_analyzer = {
         enable = true;
         installCargo = true;
         installRustc = true;
       };
-
-      # c / c++
       clangd.enable = true;
-
-      # python
       pyright.enable = true;
     };
   };
 
-  # Completions
+  # Автодополнение (nvim-cmp)
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
-    settings.sources = [
-      { name = "nvim-lsp"; }
-      { name = "path"; }
-      { name = "file"; }
-      { name = "buffer"; }
-      { name = "luasnip"; }
-    ];
+    settings = {
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
 
-    settings.window = {
-      completion = {
-        border = "rounded";
-        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None";
-        zindex = 1001;
-        scrolloff = 0;
-        colOffset = 0;
-        sidePadding = 1;
-        scrollbar = true;
+      window = {
+        completion.border = "rounded";
+        documentation.border = "rounded";
       };
-      documentation = {
-        border = "rounded";
-        winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None";
-        zindex = 1001;
-        maxHeight = 20;
+
+      mapping = {
+        "<Up>" = "cmp.mapping.select_prev_item(${selectOpts})";
+        "<Down>" = "cmp.mapping.select_next_item(${selectOpts})";
+        "<C-p>" = "cmp.mapping.select_prev_item(${selectOpts})";
+        "<C-n>" = "cmp.mapping.select_next_item(${selectOpts})";
+        "<C-u>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-d>" = "cmp.mapping.scroll_docs(4)";
+        "<C-e>" = "cmp.mapping.abort()";
+        "<C-y>" = "cmp.mapping.confirm({select = true})";
+        "<CR>" = "cmp.mapping.confirm({select = false})";
+
+        "<Tab>" = ''
+          cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" })
+        '';
+        "<S-Tab>" = ''
+          cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" })
+        '';
       };
-    };
-
-    settings.mapping = {
-      "<Up>" = "cmp.mapping.select_prev_item(${selectOpts})";
-      "<Down>" = "cmp.mapping.select_next_item(${selectOpts})";
-
-      "<C-p>" = "cmp.mapping.select_prev_item(${selectOpts})";
-      "<C-n>" = "cmp.mapping.select_next_item(${selectOpts})";
-
-      "<C-u>" = "cmp.mapping.scroll_docs(-4)";
-      "<C-d>" = "cmp.mapping.scroll_docs(4)";
-
-      "<C-e>" = "cmp.mapping.abort()";
-      "<C-y>" = "cmp.mapping.confirm({select = true})";
-      "<CR>" = "cmp.mapping.confirm({select = false})";
-
-      "<C-f>" = ''
-        cmp.mapping(
-          function(fallback)
-            if luasnip.jumpable(1) then
-              luasnip.jump(1)
-            else
-              fallback()
-            end
-          end,
-          { "i", "s" }
-        )
-      '';
-
-      "<C-b>" = ''
-        cmp.mapping(
-          function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end,
-          { "i", "s" }
-        )
-      '';
-
-      "<Tab>" = ''
-        cmp.mapping(
-          function(fallback)
-            local col = vim.fn.col('.') - 1
-
-            if cmp.visible() then
-              cmp.select_next_item(select_opts)
-            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-              fallback()
-            else
-              cmp.complete()
-            end
-          end,
-          { "i", "s" }
-        )
-      '';
-
-      "<S-Tab>" = ''
-        cmp.mapping(
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item(select_opts)
-            else
-              fallback()
-            end
-          end,
-          { "i", "s" }
-        )
-      '';
     };
   };
 
+  # Дополнительные плагины и зависимости
   plugins = {
     lazydev.enable = true;
-
-    blink-cmp.settings = {
-      sources.providers = {
-        lazydev = {
-          name = "LazyDev";
-          module = "lazydev.integrations.blink";
-          score_offset = 100;
-        };
-      };
-    };
-  };
-
-  # Misc
-  plugins = {
     luasnip.enable = true;
     cmp_luasnip.enable = true;
     cmp-nvim-lsp.enable = true;
